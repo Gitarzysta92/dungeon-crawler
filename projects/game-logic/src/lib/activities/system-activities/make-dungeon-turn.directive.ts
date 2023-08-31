ec066dcefc225f460f74d410d291bab4a076f90a
Michał Łukasiewicz <m.lukasiewic92@gmail.com>
2023-08-31 20:55:39 +0200

    Add changes for dungeon turn

--- projects/game-logic/src/lib/activities/system-activities/make-dungeon-turn.directive.ts
+++ projects/game-logic/src/lib/activities/system-activities/make-dungeon-turn.directive.ts
@@ -4,13 +4,17 @@ import { IDispatcherDirective } from "../../utils/state-dispatcher/interfaces/di
 import { SystemActivityName } from "../constants/activity-name";
 import { castEffect } from "../player-activities/cast-effect.directive";
 
+export interface UseEffectPayload {
+  effect: IEffect & (IReusable | IDisposable) & IBoardSelector,
+  targets?: (IEnemy & IBoardObject)[] | SpawnDeclaration[] | MoveDeclaration[] 
+}
 
-export const makeDungeonTurn = (payload?: {  }): IDispatcherDirective =>
+export const makeDungeonTurn = (payload?: { params: [] }): IDispatcherDirective =>
   (state: DungeonState, feed: IGameFeed) => {
      
     var activities = state.deck.cardsToUtilize.reduce((activities, card) => {
       return activities.concat(card.effects.map(e => {
-        const params = payload.params.find(p => p.id === e.id) || generateParamsForEffect(e)
+        const params = payload?.params.find(p => p.id === e.id) || generateParamsForEffect(e)
 
         if (effect.isRequired() && !params) {
           throw new Error(`There are no provided payload for effect: ${e.id}`)
